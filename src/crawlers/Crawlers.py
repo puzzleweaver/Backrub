24539ca4395053897e57d60dad4b7a63506da824
Noah Ziems <noah.ziems@live.com>
2016-10-06 08:21:21 -0400

    Starting with Crawler
    Starting with crawler

---
+++ src/crawlers/Crawlers.py
@@ -0,0 +1,22 @@
+import doc_index.Document_Index as docIndex
+import repository.Repository as repository
+import requests
+import struct
+
+def getValidText(text):
+    return text.encode('ascii', 'ignore')
+
+def run(docID):
+    url = getValidText(docIndex.getDocIDUrl(docID))
+    status, pointer, checksum, docinfo, urllen, titlelen = docIndex.getDocIndex(docID)
+    page = getValidText(requests.get(url).text)
+    print(url)
+    print(page)
+    repoPosition = repository.addDocument(docID, url, page)
+    status = 1
+    pointer = repoPosition
+    packed = struct.pack('BIIIHH', status, pointer, checksum, docinfo, urllen, titlelen)
+    docIndex.setDocIndex(docID, packed)
+    return repoPosition
+
+
